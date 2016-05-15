5caa2da9454e37a0ed2e2c0c60a6f539f6dd2d68
teekwak <teekwak@gmail.com>
2016-05-15 05:57:06 -0700

    added google map and markers

--- index.php
+++ index.php
@@ -2,8 +2,50 @@
 <html>
 	<head>
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.3/jquery.min.js"></script>
+		<style>
+	      html, body {
+	        height: 100%;
+	        margin: 0;
+	        padding: 0;
+	      }
+	      #map {
+	        height: 400px;
+	        width: 400px;
+	      }
+	    </style>
 	</head>
 	<body>
+		<div id="map"></div>
+	    <script>
+	      var map;
+	      function initMap() {
+	        map = new google.maps.Map(document.getElementById('map'), {
+	          center: {lat: 40.795358, lng: -73.95627},
+	          zoom: 11
+	        });
+
+	        var marker1 = new google.maps.Marker({
+	        	position: {lat: 40.795358, lng: -73.95627},
+	        	map: map,
+	        	icon: "http://maps.google.com/mapfiles/ms/icons/green-dot.png"
+	        });
+
+	        var marker2 = new google.maps.Marker({
+	        	position: {lat: 40.805358, lng: -73.95627},
+	        	map: map,
+	        	icon: "http://maps.google.com/mapfiles/ms/icons/red-dot.png"
+	        });
+
+	        var marker3 = new google.maps.Marker({
+	        	position: {lat: 40.785358, lng: -73.95627},
+	        	map: map,
+	        	icon: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png"
+	        });
+	      }
+	    </script>
+		<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAhOGbVuv-zvQzhnOeDNRlC8289-VzR6rw&callback=initMap" async defer></script>
+
+
 		<input class="stringTime" type="text" value="2015-09-01 04:00:00"/>
 		<button onclick="submitStringTime();">Submit</button>
 

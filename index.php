183a428fcd7599f03efe6532fea2eb0b67b4ef39
teekwak <teekwak@gmail.com>
2016-05-15 06:29:11 -0700

    working version of treemap

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html>
 	<head>
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.3/jquery.min.js"></script>
+		<script src="https://d3js.org/d3.v3.min.js" charset="utf-8"></script>
 		<style>
 	      html, body {
 	        height: 100%;
@@ -12,10 +13,98 @@
 	        height: 400px;
 	        width: 400px;
 	      }
+	      body {
+			  font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+			  margin: auto;
+			  position: relative;
+			}
+
+			form {
+			  position: absolute;
+			  right: 10px;
+			  top: 10px;
+			}
+
+			.node {
+			  border: solid 1px white;
+			  font: 10px sans-serif;
+			  line-height: 12px;
+			  overflow: hidden;
+			  position: absolute;
+			  text-indent: 2px;
+			}
+
+			.container div {
+				display: inline-block;
+			}
 	    </style>
 	</head>
 	<body>
-		<div id="map"></div>
+
+		<div class="container">
+			<div id="map"></div>
+			<div id="treemap"></div>
+		</div>
+
+		<div>
+			<input class="stringTime" type="text" value="2015-09-01 04:00:00"/>
+			<button onclick="submitStringTime();">Submit</button>
+		</div>
+
+		<script>
+			// create canvas
+			var margin = {top: 40, right: 10, bottom: 10, left: 10},
+			    width = 480 - margin.left - margin.right,
+			    height = 500 - margin.top - margin.bottom;
+
+			var color = d3.scale.category20c();
+
+			var treemap = d3.layout.treemap()
+			    .size([width, height])
+			    .sticky(true)
+			    .value(function(d) { return d.value; });
+
+			var div = d3.select("#treemap").append("div")
+			    .style("position", "relative")
+			    .style("width", (width + margin.left + margin.right) + "px")
+			    .style("height", (height + margin.top + margin.bottom) + "px")
+			    .style("left", margin.left + "px")
+			    .style("top", margin.top + "px");
+
+			d3.json("data.json", function(error, root) {
+			  if (error) throw error;
+
+			  var node = div.datum(root).selectAll(".node")
+			      .data(treemap.nodes)
+			    .enter().append("div")
+			      .attr("class", "node")
+			      .call(position)
+			      .style("background", function(d) { return d.children ? color(d.name) : null; })
+			      .text(function(d) { return d.children ? null : d.name; });
+
+			  d3.selectAll("input").on("change", function change() {
+			    var value = this.value === "count"
+			        ? function() { return 1; }
+			        : function(d) { return d.value; };
+
+			    node
+			        .data(treemap.value(value).nodes)
+			      .transition()
+			        .duration(1500)
+			        .call(position);
+			  });
+			});
+
+			function position() {
+			  this.style("left", function(d) { return d.x + "px"; })
+			      .style("top", function(d) { return d.y + "px"; })
+			      .style("width", function(d) { return Math.max(0, d.dx - 1) + "px"; })
+			      .style("height", function(d) { return Math.max(0, d.dy - 1) + "px"; });
+			}
+
+		</script>
+
+
 	    <script>
 	      var map;
 	      function initMap() {
@@ -46,8 +135,7 @@
 		<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAhOGbVuv-zvQzhnOeDNRlC8289-VzR6rw&callback=initMap" async defer></script>
 
 
-		<input class="stringTime" type="text" value="2015-09-01 04:00:00"/>
-		<button onclick="submitStringTime();">Submit</button>
+
 
 		<script>
 			var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];

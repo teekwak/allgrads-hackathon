6fd5f1280dc617a59a6bfb357bdd4edaa11cf854
teekwak <teekwak@gmail.com>
2016-05-15 04:21:34 -0700

    returning array to ajax

--- getData.php
+++ getData.php
@@ -15,8 +15,29 @@ else {
 	echo json_encode(array("status" => "FAILED: action is empty"));
 }
 
+// do logic here
 function processTimestamp() {
-	echo json_encode(array("status" => $_POST['timestamp']));
+	$tableName = "mobile_signal_info_" . $_POST['month'];
+
+
+	$mysqli = new mysqli("localhost", "root", "root", "allgrads");
+	#$sql = "INSERT INTO test (deviceId, time, mobilityStatus, latitude, longitude) VALUES (1, 2, 3, 4, 5);";
+
+	$result = $mysqli->query("SELECT * FROM TEST WHERE time > 0 AND time < 40");
+
+	$temp = array();
+	if ($result->num_rows > 0) {
+	    // output data of each row
+	    while($row = $result->fetch_assoc()) {
+	        array_push($temp, $row["deviceId"]);
+	    }
+	} else {
+
+	}
+
+	echo json_encode(array("status"=>$temp));
+
+	$mysqli->close();
 }
 
 ?>
--- index.php
+++ index.php
@@ -8,35 +8,33 @@
 		<button onclick="submitStringTime();">Submit</button>
 
 		<script>
+			var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
 			function submitStringTime() {
+				// get string from input box
 				var text = $('.stringTime').val().replace(/-/g, '/');
 
-				text = new Date(text).getTime() / 1000;
-/*
+				// get month number for picking table later
+				var mon = new Date(text).getMonth();
+
+				// convert string to timestamp
+				var ts = new Date(text).getTime() / 1000;
+
 				$.ajax({
 					type: "post",
 					url: "http://localhost:9000/getData.php",
 					data: {
 						action: "processTimestamp",
-						timestamp: text
+						month: monthNames[mon].toLowerCase(),
+						timestamp: ts
 					}
 				}).success(function(data) {
-					console.log("success! " + data.status);
+					console.log("success!");
+					console.log(data);
 				}).fail(function(jqXHR, textStatus, errorThrown) {
                     console.log("failed postSubmission: " + textStatus + " " + errorThrown);
                 });
 			}
-*/
-				$.ajax({
-					type: "post",
-					url: "http://localhost:9000/2.php",
-					data: {}
-				}).success(function(data) {
-					console.log("success: " + data.status);
-				}).fail(function(jqXHR, textStatus, errorThrown) {
-                    console.log("failed: " + textStatus + " " + errorThrown);
-                });
-
 		</script>
 	</body>
 </html>
